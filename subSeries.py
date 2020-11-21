1fd33fb9002a2119cc951fa8d035d72497a57d1e
Amir Mohammad <sohrabi2557@gmail.com>
2020-11-21 14:54:05 +0330

    comments added !

--- subSeries.py
+++ subSeries.py
@@ -1,5 +1,6 @@
 # a program to print all sub series
 
+# print the number
 def pr(number,n):
     if number != 0 :
         print("{" , str(number).replace("", " ")[1: -1] , "}")
@@ -9,6 +10,7 @@ def pr(number,n):
             if len(str(number)) != 1:
                 newnumber = newNum(number,n)
                 pr(newnumber,n)
+# to create new number
 def newNum(number,n):
     s = 0
     j = n-number%10-1
@@ -19,8 +21,10 @@ def newNum(number,n):
     result = str(number//(10)) + str(s)
     return int(result)
 
+# get the number
 n = int(input())
 
+# to create and see the result
 for j in range(1 , n+1):
     a = ''
     for i in range(j,n+1):
@@ -28,4 +32,5 @@ for j in range(1 , n+1):
     # print(a)
     pr(int(a),n)
 
+# to print empety sub serie
 print("{ }")
