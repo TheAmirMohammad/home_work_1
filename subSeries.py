6ee11aa4b42158a7f7b603d4b1e54e3afe861cea
Amir Mohammad <sohrabi2557@gmail.com>
2020-11-21 14:43:16 +0330

    defs created !

--- subSeries.py
+++ subSeries.py
@@ -1,2 +1,22 @@
 # a program to print all sub series
+
+def pr(number,n):
+    if number != 0 :
+        print("{" , str(number).replace("", " ")[1: -1] , "}")
+        if(number%10==n):
+            pr(number//10,n)
+        else:
+            if len(str(number)) != 1:
+                newnumber = newNum(number,n)
+                pr(newnumber,n)
+def newNum(number,n):
+    s = 0
+    j = n-number%10-1
+    k = j
+    for i in range(number%10+1,n+1):
+        s += i*(10**j)
+        j -= 1
+    result = str(number//(10)) + str(s)
+    return int(result)
+
 n = int(input())
